49d6c80a65819769195c7bbd61f226d0a9725f1f
MikeDre <info@mikedre.com>
2023-12-06 21:52:46 +0000

    🔧 Updated: Event list to populate dynamically, Updated dropdown styling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,61 @@
+const sample_event_data: EventItem[] = [
+  {
+    id: 1,
+    title: "Summer Party",
+    date: "7th July 2023",
+    time: "15:00"
+  },
+  {
+    id: 2, // Unique ID
+    title: "Christmas Party",
+    date: "18th Dec 2023",
+    time: "19:00"
+  },
+  {
+    id: 3, // Unique ID
+    title: "Half Marathon",
+    date: "15th Sept 2024",
+    time: "13:00"
+  }
+];
+
 type ClickTouchEvent = MouseEvent | TouchEvent;
+
+type EventItem = {
+  id: number;
+  title: string;
+  date: string;
+  time: string;
+};
 
 const $dropdown = document.getElementById('dropdown') as HTMLElement;
 const $dropdown_header = document.querySelector('.dropdown__header') as HTMLElement;
+const $dropdown__list = document.querySelector('.dropdown__list') as HTMLElement;
 
-const handleDropdown = (e: ClickTouchEvent) => {
+function handleDropdownToggle(e: ClickTouchEvent) {
   $dropdown?.classList.toggle('active');
-};
+}
 
-$dropdown_header?.addEventListener('click', handleDropdown);
+function handleDropdownItem(e: ClickTouchEvent) {
+  const target = e.target as HTMLElement;
+
+  if (target && target.classList.contains('dropdown__item')) {
+    target.classList.toggle('dropdown__item--selected');
+  }
+}
+
+function loadEvents(events: EventItem[]) {
+  if (events.length === 0) return;
+
+  events.forEach((event) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'dropdown__item';
+    listItem.innerHTML = `<strong>${event.title}</strong> - ${event.time}, ${event.date}`;
+    $dropdown__list.appendChild(listItem);
+  });
+}
+
+$dropdown_header?.addEventListener('click', handleDropdownToggle);
+$dropdown__list?.addEventListener('click', handleDropdownItem);
+
+loadEvents(sample_event_data);
