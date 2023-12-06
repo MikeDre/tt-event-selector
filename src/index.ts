62b43f3df24e7aaf0e581ed39f0530229d964706
MikeDre <info@mikedre.com>
2023-12-06 20:08:07 +0000

    ✨ Added: Core dropdown styling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-const foo = (bar: string) => {
-  return `Hello ${bar}`;
-}
+type ClickTouchEvent = MouseEvent | TouchEvent;
+
+const $dropdown = document.getElementById('dropdown') as HTMLElement;
+const $dropdown_header = document.querySelector('.dropdown__header') as HTMLElement;
+
+const handleDropdown = (e: ClickTouchEvent) => {
+  $dropdown?.classList.toggle('active');
+};
+
+$dropdown_header?.addEventListener('click', handleDropdown);
